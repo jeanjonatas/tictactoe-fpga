03bfe089f2368d99012a2aa88aa226484df2e1fc
higor-pimentel <50344120+higor-pimentel@users.noreply.github.com>
2019-06-04 17:04:24 -0300

    Update CPU.v

--- CPU.v
+++ CPU.v
@@ -1,3 +1,10 @@
 module cpu(matriz , clock);
 
 input matrix;
+
+  
+  
+  
+  
+  https://towardsdatascience.com/tic-tac-toe-creating-unbeatable-ai-with-minimax-algorithm-8af9e52c1e7d
+  http://www.codekraft.co/freecodecamp/jogo-da-velha-entendendo-o-algoritimo-minimax/
