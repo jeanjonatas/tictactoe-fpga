401ca59f2c4b47a3e4857f777d73dd7a2d924118
higor-pimentel <50344120+higor-pimentel@users.noreply.github.com>
2019-06-26 20:27:30 -0300

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -5,6 +5,7 @@ module cpu(matriz , clock, difficulty, coordenadas);
 	reg[1:0] modo;
 	parameter facil=0, medio=1, dificil=2;
 
+	//funções que definem as jogadas de acordo com a dificuldade
 	function modo_facil;
 		input blanks;
 		begin
@@ -13,7 +14,25 @@ module cpu(matriz , clock, difficulty, coordenadas);
 		modo_facil= ;
 		end
 	endfunction
+	
+	function modo_medio;
+		input blanks;
+		begin
+		
 
+		modo_medio= ;
+		end
+	endfunction
+	
+	function modo_dificil;
+		input blanks;
+		begin
+		
+
+		modo_dificil= ;
+		end
+	endfunction
+	
 
 	always @(*)begin
 		case (modo)
@@ -21,15 +40,14 @@ module cpu(matriz , clock, difficulty, coordenadas);
 			coordenadas = modo_facil(blankpos);
 			end
 			medio:begin
-			coordenadas = modo_medio;
+			coordenadas = modo_medio(blankpos);
 			end
-			dificil
-		
-		
-		
+			dificil:begin
+			coordenadas = modo_dificil(blankpos);
+			end		
 	end
 
-
+//verificação de qual a dificuldade
 	always @(posedge clock, negedge reset_n)begin
 		if(difficulty == facil)
 			modo<=facil;
@@ -41,7 +59,8 @@ module cpu(matriz , clock, difficulty, coordenadas);
 		else
 			modo<=dificil;
 			blanks<= matriz;
+		end
 	end
+	
+endmodule
   
-  https://towardsdatascience.com/tic-tac-toe-creating-unbeatable-ai-with-minimax-algorithm-8af9e52c1e7d
-  http://www.codekraft.co/freecodecamp/jogo-da-velha-entendendo-o-algoritimo-minimax/
